0a279e3bb466d60de3231bc0f66211b134247192
shashank bhushan <shaanky93@gmail.com>
2016-05-13 22:06:45 +0530

    Initial commit
    The bot is just an echo bot right now

---
+++ server.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const restify = require('restify');
+const skype = require('skype-sdk');
+
+var oConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const botService = new skype.BotService({
+    messaging: {
+        botId: oConfig.botId,
+        serverUrl : "https://apis.skype.com",
+        requestTimeout : 15000,
+        appId: oConfig.APP_ID,
+        appSecret: oConfig.APP_SECRET
+    }
+});
+
+botService.on('contactAdded', (bot, data) => {
+    bot.reply(`Hello ${data.fromDisplayName}!`, true);
+});
+
+botService.on('personalMessage', (bot, data) => {
+    bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
+});
+
+const server = restify.createServer();
+server.post('/v1/chat', skype.messagingHandler(botService));
+const port = process.env.PORT || 8080;
+server.listen(port);
+console.log('Listening for incoming requests on port ' + port);

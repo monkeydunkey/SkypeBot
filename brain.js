e19bc41f79314c89d3264da08c7f36880319256c
shashank bhushan <shaanky93@gmail.com>
2016-05-15 20:54:12 +0530

    added command recognition
    Updated the message handling structure for auto detection and routing of
    command to the respective function

--- brain.js
+++ brain.js
@@ -4,5 +4,17 @@ var oConfig = JSON.parse(fs.readFileSync('config.json', 'utf8')),
 for (var obj in oConfig.scripts) {
     oScriptLists[obj] = require(oConfig.scripts[obj]);
 }
+// This regex pattern is used to extract the command from the message
+var commandRegexPatt = / -\S+/
+var commandExtractor = function(command){
+  return commandRegexPatt.exec(command);
 
-module.exports = {oScriptLists}
+}
+var help = function(){
+  var returnString = "Following are actions I can currently perform \n";
+  for (var obj in oScriptLists) {
+    returnString = obj.help();
+  }
+  return
+}
+module.exports = {oScriptLists, commandExtractor}
--- scripts/memeGenerator.js
+++ scripts/memeGenerator.js
@@ -1,7 +1,11 @@
-var reply = function(type, topMessage, LowerMessage){
+var reply = function(command){
   return "To be implemented";
+}
+// There should be a default help option for each script
+var help = function(){
+  return "USAGE: //meme --[template] --(Top Message; Lower Message)  \n To get the templates visit - Link";
 }
 
 module.exports = {
-    reply
+    reply, help
 }
